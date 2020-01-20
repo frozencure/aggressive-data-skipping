1d722e3f9ce448d09bba63db6baf1a4bb93e5294
Verghelet Iancu <iverghelet@yahoo.com>
2020-01-20 23:54:08 +0100

    Add new parsing version

--- src/main/java/ovgu/aggressivedataskipping/queries/PredicatesController.java
+++ src/main/java/ovgu/aggressivedataskipping/queries/PredicatesController.java
@@ -3,10 +3,14 @@ package ovgu.aggressivedataskipping.queries;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/queries")
@@ -20,7 +24,13 @@ public class PredicatesController {
                                @ApiParam(value = "Path and name of the output file") @RequestParam String output,
                                @ApiParam(value = "The table restriction for the queries") @RequestParam String table) {
         QueryParser parser = new QueryParser(input, output, table);
-        return parser.exportPredicatesToJson();
+        try {
+            return parser.exportPredicatesToJson();
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Queries folder not found");
+        } catch (NullPointerException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There are no queries in the given folder");
+        }
     }
 
 }
--- src/main/java/ovgu/aggressivedataskipping/queries/QueryParser.java
+++ src/main/java/ovgu/aggressivedataskipping/queries/QueryParser.java
@@ -1,155 +1,280 @@
 package ovgu.aggressivedataskipping.queries;
 
 import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.expression.BinaryExpression;
 import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.ExpressionVisitorAdapter;
 import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
 import net.sf.jsqlparser.expression.operators.conditional.OrExpression;
+import net.sf.jsqlparser.expression.operators.relational.ComparisonOperator;
+import net.sf.jsqlparser.expression.operators.relational.InExpression;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
+
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.Hashtable;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class QueryParser {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(QueryParser.class);
+    private static final String COLUMN_NAME_KEY = "columnName";
 
-    private final static String SQL_EXTENSTION = ".sql";
+    private static final String VALUE_KEY = "value";
 
-    private String queriesFolderPath;
+    private static final String OPERATOR_KEY = "operator";
 
-    private String jsonOutputFile;
+    private static final String PREDICATES_KEY = "predicates";
+    public static final String FILE_NAME_KEY = "FileName";
+    public static final String ID_KEY = "id";
 
-    private String tableName;
 
-    public QueryParser(String queriesFolderPath, String jsonOutputFile, String tableName) {
-        this.queriesFolderPath = queriesFolderPath;
-        this.jsonOutputFile = jsonOutputFile;
-        this.tableName = tableName;
+    private JSONArray outarray = new JSONArray();
+
+    private String input;
+
+    private String output;
+
+    private String table;
+
+    public QueryParser(String input, String output, String table) {
+        this.input = input;
+        this.output = output;
+        this.table = table;
     }
 
-    private static List<File> readFiles(String folderPath) {
-        File folder = new File(folderPath);
-        File[] files = folder.listFiles((File f) -> f.getName().endsWith(SQL_EXTENSTION));
-        return Arrays.asList(files);
-    }
+    public String exportPredicatesToJson() throws IOException {
+        File folder = new File(input);
+        File[] listOfFiles = folder.listFiles();
 
-    private static boolean fileContainsString(File file, String content) {
-        try {
-            List<String> fileContent = Files.readAllLines(Paths.get(String.valueOf(file)))
-                    .stream().filter(s -> s.contains(content)).collect(Collectors.toList());
-            return fileContent.size() > 0;
-        } catch (IOException e) {
-            LOGGER.error("Query file cannot be found", e);
-        }
-        return false;
-    }
-
-    private static String getPredicatesFromSelectClause(File file) {
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            StringBuilder stringBuilder = new StringBuilder();
-            String line;
-            boolean didReadWhere = false;
-            while ((line = bufferedReader.readLine()) != null)
-                if (!didReadWhere) {
-                    if (line.startsWith("where")) didReadWhere = true;
-                } else if (!line.startsWith("go") && !line.startsWith("group") && !line.startsWith("order") && !line.startsWith("set")) {
-                    stringBuilder.append(line).append(" ");
+        Hashtable<String, JSONObject> Dict = new Hashtable<>();
+        JSONArray JSONMain = new JSONArray();
+        Path outputPath = null;
+        //atleast one file in the folder
+        if (listOfFiles != null) {
+            for (File file : listOfFiles) {
+                JSONObject Predicates = new JSONObject();
+                BufferedReader br = new BufferedReader(new FileReader(file));    //we want to access the content line wise
+                StringBuilder mainstr = new StringBuilder();
+                String st;
+                int cnt = 0;  //flag to check for the first where keyword
+                String file_content = new String(Files.readAllBytes(Paths.get(String.valueOf(file)))).replaceAll("/t", "").replaceAll(",", "");
+                if (file_content.contains(table)) {
+                    while ((st = br.readLine()) != null)
+                        if (cnt == 0) {
+                            if (st.startsWith("where")) {
+                                cnt += 1;
+                            }
+                        } else if (!st.startsWith("go") && !st.startsWith("group") && !st.startsWith("order") && !st.startsWith("set")) {
+                            mainstr.append(st).append(" ");
+                        }
                 }
-            return stringBuilder.toString();
-        } catch (IOException e) {
-            LOGGER.error("Query file cannot be found", e);
+                if (mainstr.toString().isEmpty()) {
+                    continue;
+                }
+                try {
+                    Expression expr;
+
+                    expr = CCJSqlParserUtil.parseCondExpression(mainstr.toString(), true);
+                    if (expr instanceof AndExpression) {              //check if the expression is of type AND
+                        JSONArray interobj = new JSONArray();
+                        interobj = (JSONArray) getANDPredicates(expr, file.getName());
+                        if (interobj.length() > 0) {
+                            Predicates.put("Predicates", interobj);  //gets the content inside the and block
+                        }
+                    }
+                    if (expr instanceof OrExpression) {                 //checks if expression of type OR
+                        JSONArray interobj = new JSONArray();
+                        interobj = (JSONArray) getORPredicates(expr, file.getName());
+                        if (interobj.length() > 0) {
+                            Predicates.put(PREDICATES_KEY, interobj); //gets the content inside the or block
+                        }
+                    }
+                    if (Predicates.length() > 0) {                //append each parsed files predicate along with the filename
+                        Dict.put(file.getName(), Predicates);
+                        JSONMain.put(Predicates);
+                    }
+                } catch (JSQLParserException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(outarray);
+            outputPath = Files.write(Paths.get(output), (outarray).toString().getBytes());     //json format output
         }
-        return "";
+        return outputPath.toAbsolutePath().toString();
     }
 
-    public String exportPredicatesToJson() {
-        JSONObject json = parseQueries();
-        try {
-            Path outputPath = Paths.get(jsonOutputFile);
-            Files.write(outputPath, json.toString().getBytes());
-            return outputPath.toAbsolutePath().toString();
-        } catch (IOException e) {
-            LOGGER.error("Json output path does not exist", e);
-        }
-        return "";
-    }
+    /**
+     * Handles the AND block
+     *
+     * @param expr
+     * @return JSON Object of everything inside AND block
+     * @throws JSONException
+     */
+    private Object getANDPredicates(Expression expr, String Filename) throws JSONException {
+        JSONObject ANDPredicate = new JSONObject();
+        JSONArray SimplePredicate = new JSONArray();
 
-    public JSONObject parseQueries() {
-        List<File> listOfFiles = readFiles(queriesFolderPath);
-        JSONArray jsonOutputArray = new JSONArray();
-        JsonPredicatesBuilder predicatesBuilder = new JsonPredicatesBuilder();
+        expr.accept(new ExpressionVisitorAdapter() {
 
-        for (File file : listOfFiles) {
-            JSONObject predicates = new JSONObject();
-            String predicatesString = "";
-            if (fileContainsString(file, tableName))  // if line item present
-            {
-                predicatesString = getPredicatesFromSelectClause(file);
-            }
-            System.out.println(predicatesString);
-            if (predicatesString.isEmpty()) {
-                continue;
-            }
-            try {
-                Expression expr = CCJSqlParserUtil.parseCondExpression(predicatesString, true);
-                if (expr instanceof AndExpression) {              //check if the expression is of type AND
-                    JSONObject interobj = (JSONObject) predicatesBuilder.createAndPredicates(expr);
-                    addPredicate(file, predicates, interobj);
+
+            @Override
+            protected void visitBinaryExpression(BinaryExpression expr) {
+
+                if (expr instanceof ComparisonOperator) {
+                    if ((expr.getLeftExpression().toString().startsWith("l_") && !expr.getRightExpression().toString().contains("_")) && !(expr.getRightExpression().toString().startsWith("(SELECT"))) {
+                        JSONObject PredicateObject = new JSONObject();
+                        try {
+                            PredicateObject.put(COLUMN_NAME_KEY, expr.getLeftExpression());
+                            PredicateObject.put(OPERATOR_KEY, expr.getStringExpression());
+                            if (expr.getRightExpression().toString().contains("DATE")) {
+                                PredicateObject.put(VALUE_KEY, getAdditionExpressionValue(expr.getRightExpression()));
+                            } else if (expr.getRightExpression().toString().contains("+")) {
+                                PredicateObject.put(VALUE_KEY, getAdditionValue(expr.getRightExpression()));
+                            } else {
+                                PredicateObject.put(VALUE_KEY, expr.getRightExpression());
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        SimplePredicate.put(PredicateObject);
+                    }
+
+                }
+
+
+                if (expr.getRightExpression() instanceof InExpression) {   //for handling of IN operator
+                    {
+                        if (((InExpression) expr.getRightExpression()).getLeftExpression().toString().startsWith("l_")) {
+                            List<String> list = Collections.singletonList(((InExpression) expr.getRightExpression()).getRightItemsList().toString());
+                            String val = " ";
+                            {
+                                JSONObject PredicateObject = new JSONObject();
+                                try {
+                                    PredicateObject.put(COLUMN_NAME_KEY, ((InExpression) expr.getRightExpression()).getLeftExpression());
+                                    PredicateObject.put(OPERATOR_KEY, "in");
+                                    PredicateObject.put(VALUE_KEY, ((InExpression) expr.getRightExpression()).getRightItemsList().toString());
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                SimplePredicate.put(PredicateObject);
+                            }
+
+                        }
+                    }
 
                 }
                 if (expr instanceof OrExpression) {
-                    JSONObject interobj = (JSONObject) predicatesBuilder.createOrPredicates(expr);
-                    addPredicate(file, predicates, interobj);
+                    try {
+                        getANDPredicates(expr, Filename);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
-                if (predicates.length() > 0) {
-                    //Dict.put(file.getName(),predicates);
-                    jsonOutputArray.put(predicates);
-                    System.out.println(predicates);
+                if (!(expr instanceof OrExpression)) {
+                    super.visitBinaryExpression(expr);
                 }
-            } catch (JSQLParserException | JSONException e) {
-                LOGGER.error("Error while parsing queries", e);
             }
+        });
 
-
+        if (SimplePredicate.length() > 0) {
+            ANDPredicate.put(FILE_NAME_KEY, Filename);
+            ANDPredicate.put(ID_KEY, outarray.length() + 1);
+            ANDPredicate.put(PREDICATES_KEY, SimplePredicate);
+            {
+                outarray.put(ANDPredicate);
+            }
         }
-        JSONObject jsonOutput = new JSONObject();
-        jsonOutput.put("Queries", jsonOutputArray);
-        //json format output
-//        try (Writer writer = new FileWriter("MainCsvFile.csv")) {               //csv format output
-//            writer.append("id").append(',').append("Filename").append(',').append("Predicates").append("\n");
-//            int i=1;
-//            for (Map.Entry<String, JSONObject> entry : Dict.entrySet()) {
-//                String val=entry.getValue().toString();
-//                writer.append(""+i).append(',').append(entry.getKey())
-//                        .append(',')
-//                        .append(entry.getValue().toString())
-//                        .append("\n");
-//                i++;
-//            }
-//        } catch (IOException ex) {
-//            ex.printStackTrace(System.err);
-//        }
-        return jsonOutput;
+        return SimplePredicate;
     }
 
-    private void addPredicate(File file, JSONObject predicates, JSONObject interobj) {
-        if (interobj.length() > 0) {
-            predicates.put("QueryNumber", file.getName());    //puts the query number as file name
-            predicates.put("ANDPredicates", interobj);  //gets the content inside the and block
+    private Object getORPredicates(Expression expr, String filename) throws JSONException {
+        JSONArray SimplePredicate = new JSONArray();
+        JSONObject ORPredicate = new JSONObject();
+        {
+
+            expr.accept(new ExpressionVisitorAdapter() {
+
+                @Override
+                protected void visitBinaryExpression(BinaryExpression expr) {
+
+                    if (expr instanceof ComparisonOperator) {         //only need right expression for the value tag in json file
+                        if ((expr.getLeftExpression().toString().startsWith("l_") && !expr.getRightExpression().toString().contains("_")) && !(expr.getRightExpression().toString().startsWith("(SELECT"))) {
+                            JSONObject PredicateObject = new JSONObject();
+                            try {
+                                PredicateObject.put(COLUMN_NAME_KEY, expr.getLeftExpression());
+                                PredicateObject.put(OPERATOR_KEY, expr.getStringExpression());
+                                if (expr.getRightExpression().toString().contains("DATE")) {
+                                    PredicateObject.put(VALUE_KEY, getAdditionExpressionValue(expr.getRightExpression()));
+                                } else if (expr.getRightExpression().toString().contains("+")) {
+                                    PredicateObject.put(VALUE_KEY, getAdditionValue(expr.getRightExpression()));
+                                } else {
+                                    PredicateObject.put(VALUE_KEY, expr.getRightExpression());
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            SimplePredicate.put(PredicateObject);
+                        }
+
+                    }
+                    if (expr instanceof AndExpression) {
+                        try {
+                            getANDPredicates(expr, filename);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (!(expr instanceof AndExpression)) {
+                        super.visitBinaryExpression(expr);
+                    }
+                }
+            });
         }
+        if (SimplePredicate.length() > 0) {
+            ORPredicate.put(FILE_NAME_KEY, filename);
+            ORPredicate.put(ID_KEY, outarray.length() + 1);
+            ORPredicate.put(PREDICATES_KEY, SimplePredicate);
+            outarray.put(ORPredicate);
+        }
+        return SimplePredicate;
+    }
+
+    private static String getAdditionValue(Expression expr) {
+        String Value = "";
+        String[] Numbers_Expression = expr.toString().replaceAll("'", "").split(" ");
+        if (Numbers_Expression.length > 1) {
+            Value = String.valueOf(Integer.parseInt(Numbers_Expression[0]) + Integer.parseInt(Numbers_Expression[2]));
+        } else {
+            Value = String.valueOf(Integer.parseInt(Numbers_Expression[0]));
+        }
+        return Value;
+    }
+    
+    public static String getAdditionExpressionValue(Expression expr) {
+        String result = "";
+        {
+            String[] dateExpression = expr.toString().replaceAll("'", "").split(" ");
+            LocalDate date = LocalDate.parse(dateExpression[1]);
+            if (dateExpression.length > 2) {
+                if (dateExpression[5].equals("year")) {
+                    date = date.plusYears(Integer.parseInt(dateExpression[4]));
+                } else if (dateExpression[5].equals("month")) {
+                    date = date.plusMonths(Integer.parseInt(dateExpression[4]));
+                } else if (dateExpression[5].equals("day")) {
+                    date = date.plusDays(Integer.parseInt(dateExpression[4]));
+                }
+            }
+            result = date.toString();
+        }
+        return result;
     }
 }
