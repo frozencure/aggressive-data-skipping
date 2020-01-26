9dec12f6fa53375f9cbdbf7d8086cc50fa3c2afa
Verghelet Iancu <iverghelet@yahoo.com>
2020-01-26 14:21:22 +0100

    Test augmentation

--- src/main/java/ovgu/aggressivedataskipping/augmentation/AugmentationJob.java
+++ src/main/java/ovgu/aggressivedataskipping/augmentation/AugmentationJob.java
@@ -22,23 +22,28 @@ public class AugmentationJob implements Job<Long> {
     public Long call(JobContext jobContext) throws Exception {
         SQLContext ctx = jobContext.sqlctx();
         ctx.sql("USE skipping");
-        Dataset<Row> dataset = ctx.sql("SELECT (*) FROM lineitem").withColumn("features", functions.lit(""));
-        for (int i = 0; i < featureStrings.size(); i++
-        ) {
-//            if(i==0) {
-//                Dataset<Row> returned = dataset.where(featureStrings.get(i))
-//                        .withColumn("features", functions.lit("1"));
-//                Dataset<Row> rest = dataset.except(returned)
-//                        .withColumn("features", functions.lit("0"));
-//                dataset = returned.join(rest);
-//            } else {
-            Dataset<Row> returned = dataset.where(featureStrings.get(i))
-                    .withColumn("features", dataset.col("features").plus(functions.lit("1")));
-            Dataset<Row> rest = dataset.except(returned)
-                    .withColumn("features", dataset.col("features").plus(functions.lit("0")));
-            dataset = returned.union(rest);
-//            }
-        }
+        Dataset<Row> dataset = ctx.sql("SELECT (*) FROM lineitem").withColumn("features", functions.lit(""))
+                .where(featureStrings.get(0));
+        dataset = dataset.withColumn("features", functions.col("features").plus(functions.lit("1")));
+
+//        for (int i = 0; i < featureStrings.size(); i++
+//        ) {
+////            if(i==0) {
+////                Dataset<Row> returned = dataset.where(featureStrings.get(i))
+////                        .withColumn("features", functions.lit("1"));
+////                Dataset<Row> rest = dataset.except(returned)
+////                        .withColumn("features", functions.lit("0"));
+////                dataset = returned.join(rest);
+////            } else {
+//            break;
+//            Dataset<Row> returned = dataset.where(featureStrings.get(i))
+//                    .withColumn("features", dataset.col("features").plus(functions.lit("1")));
+//            Dataset<Row> rest = dataset.except(returned)
+//                    .withColumn("features", dataset.col("features").plus(functions.lit("0")));
+//            dataset = returned.union(rest);
+//            break;
+////            }
+//        }
         dataset.write().saveAsTable("test");
         return dataset.count();
     }
